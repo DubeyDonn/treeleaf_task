a1a04aee3ac669a81ed069aa087e2fc75bd385a2
Niraj Shrestha <nirajshrestha539@gmail.com>
2024-12-09 14:26:42 +0545

    initialize spring boot application

---
+++ src/main/java/com/example/treeleaf/TreeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.treeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeleafApplication.class, args);
+	}
+
+}
